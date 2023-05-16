74f89c2e82c174ef887f747e60ed861b23ab2f65
anniemcmahon <anniemcmahon20@gmail.com>
2023-05-15 22:42:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# JavaScript Cheatsheet
+This is a place to keep all my notes about JavaScript as I am learning. I am using this project to practice my newly acquired HTML, CSS, and JavaScript skills.
+
+**Technology Used:**
+* HTML
+* CSS
+* JavaScript
+
+Web page: [https://anniemcmahon.github.io/JavaScript-cheatsheet/](https://anniemcmahon.github.io/JavaScript-cheatsheet/).
+
---
+++ script.js
@@ -0,0 +1,17 @@
+function resetAll() {
+  document.getElementById("demo").style.visibility='visible';
+  document.getElementById('demo').style.display='block';
+  document.getElementById('demo').style.color='black';
+  document.getElementById('demo').style.fontWeight='normal';
+}
+
+function boredom() {
+  let picToChange = document.getElementById("bored").src;
+  if (picToChange.indexOf('bored.jpg') != -1) {
+    document.getElementById("bored").src = "images/excited.jpg";
+    document.getElementById("bored").title = "Click again if you dare";
+  } else {
+    document.getElementById("bored").src = "images/bored.jpg";
+    document.getElementById("bored").title = "Wow, you must be really bored!";
+  }
+}
