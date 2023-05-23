59b8da7ce34178d7f39a550cb93ed8af3f0bfcb3
anniemcmahon <anniemcmahon20@gmail.com>
2023-05-22 21:31:50 -0400

    added code snippets

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function resetAll() {
-  document.getElementById("demo").style.visibility='visible';
-  document.getElementById('demo').style.display='block';
-  document.getElementById('demo').style.color='black';
-  document.getElementById('demo').style.fontWeight='normal';
+  document.getElementById("demo").style.visibility = 'visible';
+  document.getElementById('demo').style.display = 'block';
+  document.getElementById('demo').style.color = 'black';
+  document.getElementById('demo').style.fontWeight = 'normal';
 }
 
 function boredom() {
@@ -14,6 +14,69 @@ function boredom() {
     document.getElementById("bored").src = "images/bored.jpg";
     document.getElementById("bored").title = "Wow, you must be really bored!";
   }
+}
+
+function loopExamples() {
+
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 5; j++) {
+      console.log(`${j} times ${i} equal ${j * i}`);
+    }
+  }
+
+  while (num !== 3) {
+    num = Math.floor(Math.random() * 10)
+  }
+
+  do {
+    x = x + i; console.log(x); i++
+  } while (i < 5);
+
+  for (let num of numArray) {
+    console.log(num);
+  }
+
+  for (let char of myString) {
+    console.log(char)
+  }
+
+  for (let key in myObject) {
+    console.log(`${key}: ${myObject[key]}`)
+  }
+
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] === 'stop') {
+      console.log('The loop is over!');
+      break;
+    }
+    if (myArray[i] === 'next') {
+      console.log('This one is getting skipped.');
+      continue;
+    }
+    console.log(myArray[i]);
+  }
+}
+
+function objectExample() {
+  const myObject = {
+    keyOne: 'valueOne',
+    keyTwo: 'valueTwo',
+    methodOne() {
+      console.log('message');
+    }
+  };
+  let { keyOne, keyTwo } = myObject;
+  console.log(myObject[keyOne]);
+  delete myObject[keyTwo];
+  myObject.keyThree = 'new value';
 }
 
 function classExample() {
